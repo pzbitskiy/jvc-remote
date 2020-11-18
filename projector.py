cb3b11ac2ee0f66cffd6f496ed921d8f86a7e6ba
Pavel Zbitskiy <pavel.zbitskiy@gmail.com>
2020-11-18 01:02:04 -0500

    Reformat code with black

--- projector.py
+++ projector.py
@@ -1,242 +1,249 @@
 import serial
 
+
 class Button(object):
     # TODO: generate strings based on class introspection?
-    UP = 'up'
-    DOWN = 'down'
+    UP = "up"
+    DOWN = "down"
 
-    BACK = 'back'
-    ON = 'on'
-    STANDBY = 'standby'
-    INPUT = 'input'
-    BRIGHTNESS = 'brightness'
-    CONTRAST = 'contrast'
+    BACK = "back"
+    ON = "on"
+    STANDBY = "standby"
+    INPUT = "input"
+    BRIGHTNESS = "brightness"
+    CONTRAST = "contrast"
 
-    SHARPNESS = 'sharpness'
-    COLOR = 'color'
-    TINT = 'tint'
-    NR = 'noise_reduction'
-    HIDE = 'hide'
+    SHARPNESS = "sharpness"
+    COLOR = "color"
+    TINT = "tint"
+    NR = "noise_reduction"
+    HIDE = "hide"
 
-    LENS_APERTURE = 'lens_aperture'
-    MENU = 'menu'
-    OK = 'ok'
+    LENS_APERTURE = "lens_aperture"
+    MENU = "menu"
+    OK = "ok"
 
-    LENS = 'lens'
-    RIGHT = 'right'
-    LEFT = 'left'
+    LENS = "lens"
+    RIGHT = "right"
+    LEFT = "left"
 
-    TEST = 'test'
+    TEST = "test"
 
-    STAGE = 'stage'
-    CINEMA2 = 'cinema2'
-    CINEMA1 = 'cinema1'
-    NATURAL = 'natural'
-    DYNAMIC = 'dynamic'
-    USER1 = 'user1'
-    USER2 = 'user2'
-    USER3 = 'user3'
+    STAGE = "stage"
+    CINEMA2 = "cinema2"
+    CINEMA1 = "cinema1"
+    NATURAL = "natural"
+    DYNAMIC = "dynamic"
+    USER1 = "user1"
+    USER2 = "user2"
+    USER3 = "user3"
 
-    INFO = 'info'
-    GAMMA = 'gamma'
-    COLOR_TEMP = 'color_temp'
-    ASPECT = 'aspect'
+    INFO = "info"
+    GAMMA = "gamma"
+    COLOR_TEMP = "color_temp"
+    ASPECT = "aspect"
 
     CODES = {
-        UP: b'\x30\x31',
-        DOWN: b'\x30\x32',
-        BACK: b'\x30\x33',
-        ON: b'\x30\x35',
-        STANDBY: b'\x30\x36',
-        INPUT: b'\x30\x38',
-        BRIGHTNESS: b'\x30\x39',
-        CONTRAST: b'\x30\x41',
-
-        SHARPNESS: b'\x31\x34',
-        COLOR: b'\x31\x35',
-        TINT: b'\x31\x36',
-        NR: b'\x31\x38',
-        HIDE: b'\x31\x44',
-
-        LENS_APERTURE: b'\x32\x30',
-        MENU: b'\x32\x45',
-        OK: b'\x32\x46',
-
-        LENS: b'\x33\x30',
-        RIGHT: b'\x33\x34',
-        LEFT: b'\x33\x36',
-
-        TEST: b'\x35\x39',
-
-        STAGE: b'\x36\x37',
-        CINEMA2: b'\x36\x38',
-        CINEMA1: b'\x36\x39',
-        NATURAL: b'\x36\x41',
-        DYNAMIC: b'\x36\x42',
-        USER1: b'\x36\x44',
-        USER2: b'\x36\x43',
-        USER3: b'\x36\x45',
-
-        INFO: b'\x37\x34',
-        GAMMA: b'\x37\x35',
-        COLOR_TEMP: b'\x37\x36',
-        ASPECT: b'\x37\x37',
+        UP: b"\x30\x31",
+        DOWN: b"\x30\x32",
+        BACK: b"\x30\x33",
+        ON: b"\x30\x35",
+        STANDBY: b"\x30\x36",
+        INPUT: b"\x30\x38",
+        BRIGHTNESS: b"\x30\x39",
+        CONTRAST: b"\x30\x41",
+        SHARPNESS: b"\x31\x34",
+        COLOR: b"\x31\x35",
+        TINT: b"\x31\x36",
+        NR: b"\x31\x38",
+        HIDE: b"\x31\x44",
+        LENS_APERTURE: b"\x32\x30",
+        MENU: b"\x32\x45",
+        OK: b"\x32\x46",
+        LENS: b"\x33\x30",
+        RIGHT: b"\x33\x34",
+        LEFT: b"\x33\x36",
+        TEST: b"\x35\x39",
+        STAGE: b"\x36\x37",
+        CINEMA2: b"\x36\x38",
+        CINEMA1: b"\x36\x39",
+        NATURAL: b"\x36\x41",
+        DYNAMIC: b"\x36\x42",
+        USER1: b"\x36\x44",
+        USER2: b"\x36\x43",
+        USER3: b"\x36\x45",
+        INFO: b"\x37\x34",
+        GAMMA: b"\x37\x35",
+        COLOR_TEMP: b"\x37\x36",
+        ASPECT: b"\x37\x37",
     }
 
-class InputSource (object):
-    S_VIDEO = 's-video'
-    VIDEO = 'video'
-    COMPONENT = 'component'
-    COMPUTER = 'computer'
-    HDMI_1 = 'hdmi1'
-    HDMI_2 = 'hdmi2'
+
+class InputSource(object):
+    S_VIDEO = "s-video"
+    VIDEO = "video"
+    COMPONENT = "component"
+    COMPUTER = "computer"
+    HDMI_1 = "hdmi1"
+    HDMI_2 = "hdmi2"
 
     DISPLAY_NAMES = {
-        S_VIDEO: 'S-Video',
-        VIDEO: 'Video',
-        COMPONENT: 'Component',
-        COMPUTER: 'Computer',
-        HDMI_1: 'HDMI 1',
-        HDMI_2: 'HDMI 2'
+        S_VIDEO: "S-Video",
+        VIDEO: "Video",
+        COMPONENT: "Component",
+        COMPUTER: "Computer",
+        HDMI_1: "HDMI 1",
+        HDMI_2: "HDMI 2",
     }
 
-class ProjectorCommunicationError (Exception):
+
+class ProjectorCommunicationError(Exception):
     pass
+
 
 class Projector(object):
     # to be overwritten by child classes
     VALID_BUTTONS = []
     VALID_SOURCES = []
 
-    def __init__ (self, port_url, unit_id=b'\x89\x01', timeout=1):
+    def __init__(self, port_url, unit_id=b"\x89\x01", timeout=1):
         self.url = port_url
-        self.port = serial.serial_for_url (port_url,
-            19200, parity='N', stopbits=1, timeout=timeout)
+        self.port = serial.serial_for_url(
+            port_url, 19200, parity="N", stopbits=1, timeout=timeout
+        )
 
         self.unit_id = unit_id
 
-    def send_operating (self, cmd, data=None, response_cmd=None):
-        return self.send (b'\x21', cmd, data, response_cmd)
+    def send_operating(self, cmd, data=None, response_cmd=None):
+        return self.send(b"\x21", cmd, data, response_cmd)
 
-    def send_reference (self, cmd, data=None):
-        was_ok = self.send (b'\x3f', cmd, data)
+    def send_reference(self, cmd, data=None):
+        was_ok = self.send(b"\x3f", cmd, data)
 
         # we don't get any data back
         if was_ok == True:
-            return (was_ok, self.recv (cmd))
+            return (was_ok, self.recv(cmd))
         else:
             return (was_ok, None)
 
-    def send (self, header, cmd, data=None, response_cmd=None):
-        footer = b'\x0A'
+    def send(self, header, cmd, data=None, response_cmd=None):
+        footer = b"\x0A"
         if data:
             pkt = [header, self.unit_id, cmd, data, footer]
         else:
             pkt = [header, self.unit_id, cmd, footer]
 
-        self.port.write (b''.join (pkt))
+        self.port.write(b"".join(pkt))
 
         # check for the command we sent back out, unless another was specified
         if not response_cmd:
             response_cmd = cmd
 
-        return self.recv (response_cmd)
+        return self.recv(response_cmd)
 
-    def recv (self, cmd):
+    def recv(self, cmd):
         # 0x40 = response
         # 0x06 = ACK
         resp = self.port.readline()
         if not resp:
-            raise ProjectorCommunicationError ("no response from projector")
+            raise ProjectorCommunicationError("no response from projector")
 
         result_code = resp[0]
-        if result_code == b'\x06':
-            msgtype = 'ack'
-        elif result_code == b'\x40':
-            msgtype = 'data'
+        if result_code == b"\x06":
+            msgtype = "ack"
+        elif result_code == b"\x40":
+            msgtype = "data"
         else:
-            raise ProjectorCommunicationError ("device returned unknown result code %r" % result_code)
+            raise ProjectorCommunicationError(
+                "device returned unknown result code %r" % result_code
+            )
 
         # ensure the unit ID matched
         response_unit_id = resp[1:3]
         if response_unit_id != self.unit_id:
-            raise ProjectorCommunicationError ("device returned unknown unit id %r" % response_unit_id)
+            raise ProjectorCommunicationError(
+                "device returned unknown unit id %r" % response_unit_id
+            )
 
-        response_cmd = resp[3:3+len(cmd)]
+        response_cmd = resp[3 : 3 + len(cmd)]
         if response_cmd != cmd:
-            raise ProjectorCommunicationError ("device returned response command response %r for command %r" % (response_cmd, cmd))
+            raise ProjectorCommunicationError(
+                "device returned response command response %r for command %r"
+                % (response_cmd, cmd)
+            )
 
-        data = resp[3+len(cmd):-1] # don't include trailing \n
+        data = resp[3 + len(cmd) : -1]  # don't include trailing \n
 
-        if msgtype == 'data':
+        if msgtype == "data":
             return data
-        elif msgtype == 'ack':
+        elif msgtype == "ack":
             return True
-        elif msgtype == 'fail':
+        elif msgtype == "fail":
             return False
         else:
-            raise ValueError ("invalid internal msgtype")
+            raise ValueError("invalid internal msgtype")
 
     @property
-    def ready (self):
-        return self.send_operating (b'\x00\x00')
+    def ready(self):
+        return self.send_operating(b"\x00\x00")
 
     @property
     def mode(self):
-        success, state = self.send_reference (b'\x50\x57')
+        success, state = self.send_reference(b"\x50\x57")
         if not success:
             return None
 
         # note these are strings
         modes = {
-            b'\x30': 'standby',
-            b'\x31': 'power-on',
-            b'\x32': 'cool-down',
-            b'\x34': 'warning'
+            b"\x30": "standby",
+            b"\x31": "power-on",
+            b"\x32": "cool-down",
+            b"\x34": "warning",
         }
 
         if state in modes:
             return modes[state]
 
-        raise ValueError ("unknown power state " + repr(state))
+        raise ValueError("unknown power state " + repr(state))
 
     def turn_on(self):
-        return self.send_operating (b'\x50\x57', b'\x31')
+        return self.send_operating(b"\x50\x57", b"\x31")
 
     def turn_off(self):
-        return self.send_operating (b'\x50\x57', b'\x30')
+        return self.send_operating(b"\x50\x57", b"\x30")
 
     def set_input(self, mode):
         # this command is only valid when powered on
-        if self.mode != 'power-on':
+        if self.mode != "power-on":
             return False
 
         video_states = self.video_sources()
 
         if mode not in video_states:
-            return ValueError ("invalid input " + repr(mode))
+            return ValueError("invalid input " + repr(mode))
 
         cmd_state = video_states[mode]
-        return self.send_operating(b'\x49\x50', cmd_state)
+        return self.send_operating(b"\x49\x50", cmd_state)
 
     def press_button(self, btn):
         if btn not in self.VALID_BUTTONS:
-            raise ValueError ("unsupported button " + repr(btn))
+            raise ValueError("unsupported button " + repr(btn))
 
         # this command is not in standby
         # if self.mode == 'standby':
         #     return None
 
-        return self.send_operating(b'\x52\x43\x37\x33',
-            Button.CODES[btn], response_cmd=b'\x52\x43')
+        return self.send_operating(
+            b"\x52\x43\x37\x33", Button.CODES[btn], response_cmd=b"\x52\x43"
+        )
 
     @property
     def input(self):
         # this command is only valid when powered on
-        if self.mode != 'power-on':
+        if self.mode != "power-on":
             return None
 
-        success, state = self.send_reference (b'\x49\x50')
+        success, state = self.send_reference(b"\x49\x50")
         if not success:
             return None
 
@@ -246,37 +253,38 @@ class Projector(object):
         if state in video_states:
             return video_states[state]
 
-        raise ValueError ("unknown video state " + repr(state))
+        raise ValueError("unknown video state " + repr(state))
 
     @property
     def model(self):
-        return self.send_operating(b'\x4d\x44')
+        return self.send_operating(b"\x4d\x44")
 
     def video_sources(self):
         return {
-            InputSource.S_VIDEO: b'\x30',
-            InputSource.VIDEO: b'\x31',
-            InputSource.COMPUTER: b'\x32',
-            InputSource.HDMI_1: b'\x36',
-            InputSource.HDMI_2: b'\x37'
+            InputSource.S_VIDEO: b"\x30",
+            InputSource.VIDEO: b"\x31",
+            InputSource.COMPUTER: b"\x32",
+            InputSource.HDMI_1: b"\x36",
+            InputSource.HDMI_2: b"\x37",
         }
 
     def video_source_codes(self):
         return {
-            b'\x30': InputSource.S_VIDEO,
-            b'\x31': InputSource.VIDEO,
-            b'\x32': InputSource.COMPUTER,
-            b'\x36': InputSource.HDMI_1,
-            b'\x37': InputSource.HDMI_2
+            b"\x30": InputSource.S_VIDEO,
+            b"\x31": InputSource.VIDEO,
+            b"\x32": InputSource.COMPUTER,
+            b"\x36": InputSource.HDMI_1,
+            b"\x37": InputSource.HDMI_2,
         }
 
-class HD250 (Projector):
+
+class HD250(Projector):
     VALID_SOURCES = [
         InputSource.S_VIDEO,
         InputSource.VIDEO,
         InputSource.COMPUTER,
         InputSource.HDMI_1,
-        InputSource.HDMI_2
+        InputSource.HDMI_2,
     ]
 
     VALID_BUTTONS = [
@@ -315,9 +323,9 @@ class HD250 (Projector):
     ]
 
     SOURCES_MAP = {
-        InputSource.COMPONENT: b'\x32',
-        InputSource.HDMI_1: b'\x36',
-        InputSource.HDMI_2: b'\x37'
+        InputSource.COMPONENT: b"\x32",
+        InputSource.HDMI_1: b"\x36",
+        InputSource.HDMI_2: b"\x37",
     }
 
     SOURCES_CODES = {code: name for name, code in SOURCES_MAP.items()}
@@ -330,15 +338,11 @@ class HD250 (Projector):
 
     @property
     def model(self):
-        return 'DLA-HD250'
+        return "DLA-HD250"
 
 
-class RS40 (Projector):
-    VALID_SOURCES = [
-        InputSource.COMPONENT,
-        InputSource.HDMI_1,
-        InputSource.HDMI_2
-    ]
+class RS40(Projector):
+    VALID_SOURCES = [InputSource.COMPONENT, InputSource.HDMI_1, InputSource.HDMI_2]
 
     VALID_BUTTONS = [
         Button.UP,
@@ -368,11 +372,11 @@ class RS40 (Projector):
     ]
 
     SOURCES_MAP = {
-        InputSource.S_VIDEO: b'\x30',
-        InputSource.VIDEO: b'\x31',
-        InputSource.COMPUTER: b'\x32',
-        InputSource.HDMI_1: b'\x36',
-        InputSource.HDMI_2: b'\x37'
+        InputSource.S_VIDEO: b"\x30",
+        InputSource.VIDEO: b"\x31",
+        InputSource.COMPUTER: b"\x32",
+        InputSource.HDMI_1: b"\x36",
+        InputSource.HDMI_2: b"\x37",
     }
 
     SOURCES_CODES = {code: name for name, code in SOURCES_MAP.items()}
@@ -385,18 +389,19 @@ class RS40 (Projector):
 
     @property
     def model(self):
-        return 'DLA-RS40'
+        return "DLA-RS40"
 
-if __name__ == '__main__':
-    p = HD250 ('/dev/ttyUSB0')
+
+if __name__ == "__main__":
+    p = HD250("/dev/ttyUSB0")
 
     if p.ready:
-        print ("currently in", p.mode, "mode")
-        print ("viewing input", p.input)
-        print ("model", p.model)
+        print("currently in", p.mode, "mode")
+        print("viewing input", p.input)
+        print("model", p.model)
 
-        p.press_button (Button.BACK) # dismiss lamp warning
+        p.press_button(Button.BACK)  # dismiss lamp warning
         # p.turn_on()
         # time.sleep (35) # let the projector warm up
     else:
-        print ("device not ready yet")
+        print("device not ready yet")
--- server.py
+++ server.py
@@ -10,141 +10,164 @@ from projector import Button, InputSource, RS40, ProjectorCommunicationError
 
 projector = None
 
-class WebException (Exception):
-    def __init__ (self, code, msg=None):
+
+class WebException(Exception):
+    def __init__(self, code, msg=None):
         self.code = code
         default_msgs = {
-            '404 Not Found': 'The requested resource could not be found.',
-            '503 Service Unavailable': 'The underlying service is not available.',
-            '500 Internal Server Error': 'An internal exception occurred.'
+            "404 Not Found": "The requested resource could not be found.",
+            "503 Service Unavailable": "The underlying service is not available.",
+            "500 Internal Server Error": "An internal exception occurred.",
         }
 
         if not msg:
             if code in default_msgs:
                 self.msg = default_msgs[code]
             else:
-                self.msg = ''
+                self.msg = ""
         else:
             self.msg = msg
 
-    def __repr__ (self):
+    def __repr__(self):
         if not self.msg:
             return self.code
-        return '%s: %s' % (self.code, self.msg)
+        return "%s: %s" % (self.code, self.msg)
 
-    def __str__ (self):
-        return '<h1>%s</h1><p>%s</p>' % (self.code, self.msg)
+    def __str__(self):
+        return "<h1>%s</h1><p>%s</p>" % (self.code, self.msg)
 
-def projector_command (f):
+
+def projector_command(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         try:
             return f(*args, **kwargs)
         except ProjectorCommunicationError:
-            raise WebException ('503 Service Unavailable', 'Could not '
+            raise WebException(
+                "503 Service Unavailable",
+                "Could not "
                 'communicate with projector "%s". Is it connected and '
-                'powered on?' % projector.url)
+                "powered on?" % projector.url,
+            )
+
     return decorated
+
 
 # information only (this may change if we
 # implement an auto-instanciated projector class)
 def view_buttons():
-    return json.dumps({'names': projector.VALID_BUTTONS})
+    return json.dumps({"names": projector.VALID_BUTTONS})
+
 
 def view_inputs():
-    return json.dumps(dict(zip (projector.VALID_SOURCES,
-        map (lambda src: InputSource.DISPLAY_NAMES[src],
-                         projector.VALID_SOURCES))))
+    return json.dumps(
+        dict(
+            zip(
+                projector.VALID_SOURCES,
+                map(
+                    lambda src: InputSource.DISPLAY_NAMES[src], projector.VALID_SOURCES
+                ),
+            )
+        )
+    )
+
 
 @projector_command
 def press(button):
     button = button.lower()
     if button not in Button.CODES:
-        raise WebException ('404 Not Found', 'No such button ' + button)
+        raise WebException("404 Not Found", "No such button " + button)
 
-    return json.dumps({'success': projector.press_button (button)})
+    return json.dumps({"success": projector.press_button(button)})
+
 
 @projector_command
 def projector_status():
-    return json.dumps({
-        'mode': projector.mode,
-        'input': projector.input,
-        'model': projector.model
-    })
+    return json.dumps(
+        {"mode": projector.mode, "input": projector.input, "model": projector.model}
+    )
+
 
 @projector_command
 def set_input(source):
     source = source.lower()
     if source not in projector.VALID_SOURCES:
-        raise WebException ('404 Not Found', 'Invalid mode ' + source)
+        raise WebException("404 Not Found", "Invalid mode " + source)
 
-    return json.dumps({'success': projector.set_input(source)})
+    return json.dumps({"success": projector.set_input(source)})
+
 
 @projector_command
 def on():
-    return json.dumps({'success': projector.turn_on()})
+    return json.dumps({"success": projector.turn_on()})
+
 
 @projector_command
 def off():
-    return json.dumps({'success': projector.turn_off()})
+    return json.dumps({"success": projector.turn_off()})
+
 
 def index():
-    return open ('index.html', 'rt').read()
+    return open("index.html", "rt").read()
 
-def remote_webapp (environ, start_response):
+
+def remote_webapp(environ, start_response):
     routes = {
-        'buttons': view_buttons,
-        'inputs': view_inputs,
-        'press': lambda: press(shift_path_info (environ)),
-        'status': projector_status,
-        'input': lambda: set_input(shift_path_info (environ)),
-        'on': on,
-        'off': off,
-        '': index
+        "buttons": view_buttons,
+        "inputs": view_inputs,
+        "press": lambda: press(shift_path_info(environ)),
+        "status": projector_status,
+        "input": lambda: set_input(shift_path_info(environ)),
+        "on": on,
+        "off": off,
+        "": index,
     }
 
     result = None
-    base_path = shift_path_info (environ)
+    base_path = shift_path_info(environ)
     if base_path not in routes:
-        status = '404 Not Found'
-        headers = [('Content-type', 'text/html')]
-        result = str(WebException (status))
+        status = "404 Not Found"
+        headers = [("Content-type", "text/html")]
+        result = str(WebException(status))
     else:
         try:
             handler = routes[base_path]
             result = handler()
-            status = '200 OK'  # HTTP Status
+            status = "200 OK"  # HTTP Status
             if handler == index:
-                headers = [('Content-type', 'text/html')]
+                headers = [("Content-type", "text/html")]
             else:
-                headers = [('Content-type', 'application/json')]
+                headers = [("Content-type", "application/json")]
         except WebException as ex:
             status = ex.code
-            headers = [('Content-type', 'text/html')]
+            headers = [("Content-type", "text/html")]
             result = str(ex)
         except:
-            status = '500 Internal Server Error'
-            headers = [('Content-type', 'text/html')]
+            status = "500 Internal Server Error"
+            headers = [("Content-type", "text/html")]
             e_type, e_value, tb = exc_info()
-            html = ('An internal exception occured. The stacktrace was: '
-                   '</p><pre>%s\n%s</pre><p>' % (
-                        ''.join (format_tb(tb)),
-                        '%s: %s' % (e_type.__name__, e_value)))
-            result = str(WebException (status, html))
+            html = (
+                "An internal exception occured. The stacktrace was: "
+                "</p><pre>%s\n%s</pre><p>"
+                % ("".join(format_tb(tb)), "%s: %s" % (e_type.__name__, e_value))
+            )
+            result = str(WebException(status, html))
 
     start_response(status, headers)
 
-    result = result.encode('utf8')
+    result = result.encode("utf8")
     return [result]
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
+
     if len(sys.argv) != 2:
-        sys.stderr.write ("Usage: %s [serial device]\n" % sys.argv[0])
+        sys.stderr.write("Usage: %s [serial device]\n" % sys.argv[0])
     else:
-        projector = RS40 (sys.argv[1], timeout=0.4)
-        httpd = make_server('', 8000, remote_webapp)
-        print ("Serving on port 8000...")
+        projector = RS40(sys.argv[1], timeout=0.4)
+        httpd = make_server("", 8000, remote_webapp)
+        print("Serving on port 8000...")
 
         # Serve until process is killed
         httpd.serve_forever()
